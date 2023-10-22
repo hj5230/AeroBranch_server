720626b4a7d800137a0aca60464f509abb25d15a
hj5230 <hf305@foxmail.com>
2023-10-23 00:46:53 +0800

    add model

---
+++ src/model/Chunk.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema, InferSchemaType, Model } from "mongoose";
+
+const ChunkSchema: Schema = new Schema({
+  chunkId: {
+    type: Number,
+    required: true
+  },
+  chunkNo: {
+    type: Number,
+    required: true
+  },
+  data: {
+    type: Buffer,
+    required: true
+  }
+});
+
+const Chunk: Model<InferSchemaType<typeof ChunkSchema>> = mongoose.model('Chunk', ChunkSchema);
+
+export default Chunk;
