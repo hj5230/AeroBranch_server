c4a21d440548e23dc29c2d673566d0373676f7e8
hj5230 <hf305@foxmail.com>
2023-10-23 00:13:11 +0800

    add repository model

---
+++ src/model/Repository.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, InferSchemaType, Model } from 'mongoose';
+
+const RepoSchema: Schema = new Schema({
+  repoId: {
+    type: Number,
+    required: true,
+  },
+  repoName: {
+    type: String,
+    required: true,
+  },
+  belongTo: {
+    type: Number,
+    required: true,
+  },
+  files: {
+    type: [Number],
+  },
+});
+
+const RepoModel: Model<InferSchemaType<typeof RepoSchema>> = mongoose.model(
+  'Repository',
+  RepoSchema,
+);
+
+export default RepoModel;
