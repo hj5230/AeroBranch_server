51ad4ffdca051ad4d4b6d790ae34d70db37321c1
hj5230 <hf305@foxmail.com>
2024-06-23 20:46:49 +0800

    feat(log): use winston as logger

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,30 @@
+/**
+ * Main entry point of the server application.
+ * It imports necessary modules, sets up middleware, connects to MongoDB,
+ * generates demo records if none exists, and starts the server.
+ */
+
 import Koa from "koa";
 import path from "path";
-// import https from "https";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-// import fs from "fs";
+import winston from "winston";
 
-// import middlewares
+/* import middlewares */
 import { bodyParserMiddleware } from "./middleware/bodyParserMiddleware";
 import { corsMiddleware } from "./middleware/corsMiddleware";
-// import { jwtMiddleware } from "./middleware/jwtMiddleware";
 
-// import routes
+/* import routes */
 import mainRoutes from "./routes/main";
 import testRoutes from "./routes/test";
 import UserRoutes from "./routes/user";
 
-// import models
+/* import models */
 import User from "./model/User";
 import Device from "./model/Device";
-// import Repository from './model/Repository';
-// import File from './model/File';
 
-// config dotenv
-dotenv.config({
-    path: path.resolve(__dirname, "../.env"),
-    encoding: "utf8",
-    debug: false,
-}).parsed;
-
-// config https certificate
-// interface PemOptions {
-//     key: Buffer;
-//     cert: Buffer;
-// }
-
-// const options: PemOptions = {
-//     key: fs.readFileSync("./key.pem"),
-//     cert: fs.readFileSync("./cert.pem"),
-// };
-
-// handle exceptions
+/* handle exceptions */
 process.on("unhandledRejection", (reason, promise) => {
     console.error("Unhandled Rejection at:", promise, "reason:", reason);
 });
@@ -49,15 +33,56 @@ process.on("uncaughtException", (reason, exception) => {
     console.error("Unhandled Rejection at:", exception, "reason:", reason);
 });
 
-// initialize mongodb
-const connInfo = process.env.MONGODB_URI || "";
-mongoose.set("strictQuery", true);
-mongoose
-    .connect(connInfo)
-    .then(() => console.log("Connected to MongoDB"))
-    .catch(err => console.error("Failed to connect to MongoDB:", err));
+/**
+ * Create logger instance for logging application events.
+ * Override console stdout methods to use the logger.
+ */
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+        }),
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: "app.log" }),
+    ],
+});
+console.log = (msg, ...opts) => logger.info(msg, ...opts);
+console.warn = (msg, ...opts) => logger.warn(msg, ...opts);
+console.error = (msg, ...opts) => logger.error(msg, ...opts);
 
-// generate demo records into db
+/* config dotenv */
+dotenv.config({
+    path: path.resolve(__dirname, "../.env"),
+    encoding: "utf8",
+    debug: false,
+}).parsed;
+
+/**
+ * Connects to MongoDB using the provided connection string.
+ * Sets "strictQuery" option to true.
+ * Logs a success / error message regarding if connection is successful.
+ */
+const connectToMongoDB = async () => {
+    const connInfo = process.env.MONGODB_URI || "";
+    mongoose.set("strictQuery", true);
+    try {
+        await mongoose.connect(connInfo);
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.error("Failed to connect to MongoDB:", err);
+    }
+};
+
+connectToMongoDB();
+
+/**
+ * Generates demo records in the database if no user exists.
+ * Creates a default user with a hashed password and a device.
+ */
 const createDefaultUserIfNoneExists = async () => {
     const userCount = await User.countDocuments();
     if (userCount === 0) {
@@ -75,7 +100,7 @@ const createDefaultUserIfNoneExists = async () => {
                 }).save();
                 new Device({
                     deviceId: initId,
-                    deviceName: "WUJIE-14",
+                    deviceName: process.env.DEV_HOST_NAME,
                     macAddress: process.env.DEV_HOST_MAC,
                     belongTo: initId,
                 }).save();
@@ -83,20 +108,19 @@ const createDefaultUserIfNoneExists = async () => {
         });
     }
 };
+
 createDefaultUserIfNoneExists();
 
 const app: Koa = new Koa();
 
-// middleware
+/* register middlewares */
 app.use(bodyParserMiddleware);
 app.use(corsMiddleware);
-// app.use(jwtMiddleware);
 
-// routes
+/* register routes */
 app.use(mainRoutes.routes()).use(mainRoutes.allowedMethods());
 app.use(testRoutes.routes()).use(testRoutes.allowedMethods());
 app.use(UserRoutes.routes()).use(UserRoutes.allowedMethods());
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
-// https.createServer(options, app.callback()).listen(PORT, () => console.log(`https://localhost:${PORT}`));
